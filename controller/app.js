1f5c3590856723be2525ee50fd573c8aacd17465
meiroo <meiroo@outlook.com>
2015-01-05 15:50:25 +0800

    up angular js

---
+++ controller/app.js
@@ -0,0 +1,57 @@
+define(function(require, exports, module) {
+
+    // var ImageSpace = require('controller/ImageSpace');
+    var angular = require('angular');
+   
+    var app = angular.module("Index", ['ngRoute'], function() {
+    	
+    });
+
+
+
+    app.config(function($routeProvider, $locationProvider) {
+
+      $routeProvider
+      .when('/about', {
+	    templateUrl: 'template/about.html'
+	   })
+	  .when('/:pathName*', {
+	    templateUrl: 'template/imagespace.html',
+	    controller: 'ImageSpaceController'
+	   })
+	  .when('/', {
+	    templateUrl: 'template/imagespace.html',
+	    controller: 'ImageSpaceController'
+	   })
+       ;
+	   
+	});
+
+    app.factory('Data', function() {
+	  return {
+	  	url:'/',
+	  	refresh:0,
+	  	itemdelete: null,
+	  	forceRefresh:function(){this.refresh++;},
+	  	setURL:function(url){
+	  		this.url=url;
+	  	},
+	  	setItemDelete:function(item){this.itemdelete = item;},	  	
+	  };
+	});
+
+
+
+    //app.controller('TitleController',TitleController);    
+    //app.controller('newfolderController',NewfolderController);
+    //app.controller('rmfolderController',RmfolderController);
+    //app.controller('ImageSpaceController',ImageSpaceController);
+
+	module.exports = app;
+
+
+
+	
+	
+});
+
---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+require.config({
+    baseUrl: '',
+    paths: {
+        angular: "js/angular",
+        "angular-route": "js/angular-route",
+        app:'controller/app',
+
+    },
+    shim: {
+        angular: {
+            exports: "angular"
+        },
+        "angular-route":{
+            exports:"angular-route",
+            deps: ["angular"]
+        }
+      
+
+    }
+});
+
+window.name = "NG_DEFER_BOOTSTRAP!";
+
+$(document).ready(function() {
+    require(['angular','angular-route','app'],function(angular,route,app){
+        var $html = angular.element(document.getElementsByTagName('html')[0]);
+
+        angular.element().ready(function() {
+            angular.resumeBootstrap([app['name']]);
+        });
+    });
+});
