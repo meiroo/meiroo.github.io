61e84d4fe24da9ec1d307cbc9630deb6c047c530
meiroo <meiroo@outlook.com>
2015-01-14 10:35:12 +0800

    up

--- js/movement.js
+++ js/movement.js
@@ -6,24 +6,24 @@ function Page(str){
 
 	this.hammertime.on('swipeleft', function(ev) {
 	   pan -= 320;
-	   TweenMax.staggerTo("#"+str+" .imglist img", 1, {x:pan}, 0.0);
+	   TweenMax.staggerTo("#"+str+" .imglist img", 0.5, {x:pan}, 0.0);
 	});
 
 	this.hammertime.on('swiperight', function(ev) {
 		pan += 320;
-	   TweenMax.staggerTo("#"+str+" .imglist img", 1, {x:pan}, 0.0);
+	   TweenMax.staggerTo("#"+str+" .imglist img", 0.5, {x:pan}, 0.0);
 	});
 
 	this.showAni = function(){
 		this.ele.show();
-		TweenMax.fromTo("#"+str, 1, {rotation:30,y:300},{rotation:0, y:0});
-		TweenMax.fromTo("#"+str+" .pure-img", 1, {rotation:0},{rotation:360,repeat:-1,repeatDelay:0.5,ease:Back.easeOut});
-		TweenMax.fromTo("#"+str+" .pure-u-1-3", 1, {scale:0},{scale:1,ease:Bounce.easeOut});
+		TweenMax.fromTo("#"+str, 0.5, {rotation:30,y:300},{rotation:0, y:0});
+		TweenMax.fromTo("#"+str+" .pure-img", 0.5, {rotation:0},{rotation:360,repeat:-1,repeatDelay:0.5,ease:Back.easeOut});
+		TweenMax.fromTo("#"+str+" .pure-u-1-3", 0.5, {scale:0},{scale:1,ease:Bounce.easeOut});
 		
 	}
 
 	this.hideAni = function(onComplete){
-		TweenMax.fromTo("#"+str, 1, {rotation:0,y:-300},{rotation:-30, y:-1000,onComplete:onComplete});
+		TweenMax.fromTo("#"+str, 0.5, {rotation:0,y:-300},{rotation:-30, y:-1000,onComplete:onComplete});
 	}
 
 }
