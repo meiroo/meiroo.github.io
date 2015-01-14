9cdbdc907225bd7bef1eb52afa1cd60b7847edad
meiroo <meiroo@outlook.com>
2015-01-14 16:10:30 +0800

    up

--- js/movement.js
+++ js/movement.js
@@ -1,3 +1,23 @@
+var browser={
+	versions:function(){
+        var u = navigator.userAgent, app = navigator.appVersion;
+        return {         //移动终端浏览器版本信息
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+            iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
+            iPad: u.indexOf('iPad') > -1, //是否iPad
+            webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+        };
+     }(),
+     language:(navigator.browserLanguage || navigator.language).toLowerCase()
+}
+	
+
 function Page(str){
 	this.ele = $('#'+str);
 	var pan = 0;
@@ -46,7 +66,7 @@ function Scene(){
 		pages[currentindex].showAni();
 
 	
-		$('body').hammer({direction: Hammer.DIRECTION_ALL }).bind("panup swipeup", function(ev) {
+		$('body').hammer().bind("panup swipeup", function(ev) {
 		   ev.preventDefault();
 		   pages[currentindex].hideAni(function(){
 		   		pages[currentindex].ele.hide();
@@ -93,24 +113,7 @@ function Scene(){
 
 $(document).ready(function() {
 
-	 var browser={
-    	versions:function(){
-            var u = navigator.userAgent, app = navigator.appVersion;
-            return {         //移动终端浏览器版本信息
-                trident: u.indexOf('Trident') > -1, //IE内核
-                presto: u.indexOf('Presto') > -1, //opera内核
-                webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-                mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
-                iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
-                iPad: u.indexOf('iPad') > -1, //是否iPad
-                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
-            };
-         }(),
-         language:(navigator.browserLanguage || navigator.language).toLowerCase()
-	}
+	 
 
 	var mm = new Scene();
 	mm.init();
